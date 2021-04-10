470644ff1b147234d02c532eaf0ed26f8e7cb887
cgiovanni96 <cgiovanni1996@gmail.com>
2021-04-10 14:45:51 +0200

    server: feat: commented graphql server, focusing on basic webRTC functionalities for now

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,29 @@
 import Fastify from 'fastify'
-import mercurius from 'mercurius'
+// import mercurius from 'mercurius'
 
 const app = Fastify()
 
-const schema = `
-  type Query {
-    add(x: Int, y: Int): Int
-  }
-`
+// const schema = `
+//   type Query {
+//     add(x: Int, y: Int): Int
+//   }
+// `
 
-const resolvers = {
-	Query: {
-		add: async () => 2
-	}
-}
+// const resolvers = {
+// 	Query: {
+// 		add: async () => 2
+// 	}
+// }
 
-app.register(mercurius, {
-	schema,
-	resolvers
-})
+// app.register(mercurius, {
+// 	schema,
+// 	resolvers
+// })
 
 app.get('/', async function () {
 	return 'hello'
 })
 
-app.listen(5000)
+app.listen(5000, () => {
+	console.log('Serve Ready at port 5000')
+})
